e3ae20b459863f4ba4938e6d5a398b6112e65e1f
LlucVelazquez <lluc.velazquez.7e8@itb.cat>
2025-02-10 13:05:24 +0100

    nose

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/GameScreen.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/GameScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
@@ -47,6 +48,7 @@ fun GameScreen(navigateToResultScreen: (Int)-> Unit){
                 Text("Answer4")
             }
         }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) { LinearProgressIndicator() }
     }
     if(num  > 10){
         navigateToResultScreen(score)
